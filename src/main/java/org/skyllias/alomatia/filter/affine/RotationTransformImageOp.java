b6a431418751f9bb23b622ba843f368e6c3fb70c
Skyllias <31156063+skyllias@users.noreply.github.com>
2017-11-12 10:26:49 +0100

    BufferedImageOps

---
+++ src/main/java/org/skyllias/alomatia/filter/affine/RotationTransformImageOp.java
@@ -0,0 +1,22 @@
+
+package org.skyllias.alomatia.filter.affine;
+
+import java.awt.geom.*;
+import java.awt.image.*;
+
+/** {@link BufferedImageOp} that rotates the image 180º.
+ *  Other angles would not generally produce images with the same size. */
+
+public class RotationTransformImageOp extends FixedSizeAffineTransformImageOp
+{
+//==============================================================================
+
+  @Override
+  protected AffineTransform getTransform(int width, int height)
+  {
+    return AffineTransform.getQuadrantRotateInstance(2, ((double) width) / 2, ((double) height) / 2);
+  }
+
+//------------------------------------------------------------------------------
+
+}
