aaefd709efd5ef9a74fe435e55af4f4525811d69
Skyllias <31156063+skyllias@users.noreply.github.com>
2018-02-17 12:14:51 +0100

    Use RecursiveImageAction in SurroundingColoursOp

--- src/main/java/org/skyllias/alomatia/filter/buffered/SurroundingColoursOp.java
+++ src/main/java/org/skyllias/alomatia/filter/buffered/SurroundingColoursOp.java
@@ -6,6 +6,8 @@ import java.awt.image.*;
 import java.util.*;
 import java.util.concurrent.*;
 
+import org.skyllias.alomatia.filter.buffered.parallelization.*;
+
 /** {@link BufferedImageOp} that replaces the colour in each pixel with another
  *  one derived from all the pixels in the box surrounding it.
  *  The alpha channel info is erased.
@@ -41,12 +43,10 @@ public class SurroundingColoursOp extends BasicBufferedImageOp
   @Override
   public void doFilter(BufferedImage src, BufferedImage dest)
   {
-    int imageWidth      = src.getWidth();
-    int imageHeight     = src.getHeight();
     Color[] colourCache = getAllPixels(src);
 
-    RecursiveColoursAction processAction = new RecursiveColoursAction(dest, colourCache, 0,
-                                                                      imageWidth, 0, imageHeight);
+    SurroundingColoursProcessor imageProcessor = new SurroundingColoursProcessor(colourCache, dest);
+    RecursiveAction processAction              = new RecursiveImageAction(imageProcessor);
     processAction.invoke();
   }
 
@@ -97,74 +97,30 @@ public class SurroundingColoursOp extends BasicBufferedImageOp
 
 //******************************************************************************
 
-  /* Fork/join task that recursively splits the image whose colours are to be
-   * processed into halves, until they are 100x100 or smaller. Then, the
-   * fragments are scanned, retrieving all the surrounding colours of each
-   * pixel (which may or may not lie inside the current fragment) and using
-   * calculator to set the colour in the destination image. */
-
-  @SuppressWarnings("serial")
-  private class RecursiveColoursAction extends RecursiveAction
+  private class SurroundingColoursProcessor implements ImageProcessor
   {
-    private BufferedImage dest;
     private Color[] srcColours;
-    private int xFrom, xTo, yFrom, yTo;
+    private BufferedImage destImage;
 
-    /* Creates a task that will process the pixels inside the rectangle
-     * between (xFrom, yFrom) (inclusive) and (xTo, yTo) (exclusive). */
-
-    public RecursiveColoursAction(BufferedImage destImage, Color[] srcColours,
-                                  int xFrom, int xTo, int yFrom, int yTo)
+    public SurroundingColoursProcessor(Color[] srcColours, BufferedImage destImage)
     {
-      this.dest       = destImage;
       this.srcColours = srcColours;
-      this.xFrom      = xFrom;
-      this.xTo        = xTo;
-      this.yFrom      = yFrom;
-      this.yTo        = yTo;
+      this.destImage  = destImage;
     }
-
-    /* First, the image is split horizontally until stripes of 100 or less pixels
-     * are obtained; afterwards, it is split vertically with the same criteria.
-     * If the task's fragment is small enough, the real computation is carried out. */
 
     @Override
-    protected void compute()
+    public int getImageWidth() {return destImage.getWidth();}
+
+    @Override
+    public int getImageHeight() {return destImage.getHeight();}
+
+    @Override
+    public void processPixel(int x, int y)
     {
-      final int MAX_SIZE = 100;
-
-      if (xTo - xFrom > MAX_SIZE)
-      {
-        final int halfWay = (xTo - xFrom) / 2;
-        RecursiveColoursAction leftAction  = new RecursiveColoursAction(dest, srcColours, xFrom, xFrom + halfWay, yFrom, yTo);
-        RecursiveColoursAction rightAction = new RecursiveColoursAction(dest, srcColours, xFrom + halfWay, xTo, yFrom, yTo);
-        invokeAll(leftAction, rightAction);
-      }
-      else if (yTo - yFrom > MAX_SIZE)
-      {
-        final int halfWay = (yTo - yFrom) / 2;
-        RecursiveColoursAction topAction    = new RecursiveColoursAction(dest, srcColours, xFrom, xTo, yFrom, yFrom + halfWay);
-        RecursiveColoursAction bottomAction = new RecursiveColoursAction(dest, srcColours, xFrom, xTo, yFrom + halfWay, yTo);
-        invokeAll(topAction, bottomAction);
-      }
-      else processPixels();
-    }
-
-    /* Processes the pixels inside the fragment, updating the destination image
-     * according to the source colours. */
-
-    private void processPixels()
-    {
-      for (int i = xFrom; i < xTo; i++)
-      {
-        for (int j = yFrom; j < yTo; j++)
-        {
-          Collection<Color> surroundingColours = getSurroundingColours(srcColours, i, j,
-                                                                       dest.getWidth(), dest.getHeight());
-          Color pickedColour                   = calculator.getColour(surroundingColours);
-          dest.setRGB(i, j, pickedColour.getRGB());
-        }
-      }
+      Collection<Color> surroundingColours = getSurroundingColours(srcColours, x, y,
+                                                                   destImage.getWidth(), destImage.getHeight());
+      Color pickedColour                   = calculator.getColour(surroundingColours);
+      destImage.setRGB(x, y, pickedColour.getRGB());
     }
   }
 }
