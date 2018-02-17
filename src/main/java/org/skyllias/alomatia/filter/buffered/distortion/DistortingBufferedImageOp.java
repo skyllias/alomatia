840969fe475ad8df5d1269d900e71a6f2aab0708
Skyllias <31156063+skyllias@users.noreply.github.com>
2018-02-17 12:29:34 +0100

    Using RecursiveImageAction to distort #50

--- src/main/java/org/skyllias/alomatia/filter/buffered/distortion/DistortingBufferedImageOp.java
+++ src/main/java/org/skyllias/alomatia/filter/buffered/distortion/DistortingBufferedImageOp.java
@@ -4,8 +4,10 @@ package org.skyllias.alomatia.filter.buffered.distortion;
 import java.awt.*;
 import java.awt.geom.*;
 import java.awt.image.*;
+import java.util.concurrent.*;
 
 import org.skyllias.alomatia.filter.buffered.*;
+import org.skyllias.alomatia.filter.buffered.parallelization.*;
 
 /** {@link BasicBufferedImageOp} that distorts images by applying a
  *  {@link Distortion} to all pixels. */
@@ -28,21 +30,43 @@ public class DistortingBufferedImageOp extends BasicBufferedImageOp
   @Override
   protected void doFilter(BufferedImage src, BufferedImage dest)
   {
-    Dimension2D imageBounds = new Dimension(dest.getWidth(), dest.getHeight());
-
-    for (int i = 0; i < dest.getWidth(); i++)
-    {
-      for (int j = 0; j < dest.getHeight(); j++)
-      {
-        Point2D.Float currentDestPixel = new Point2D.Float(i + PixelizationConstants.PIXEL_OFFSET,
-                                                           j + PixelizationConstants.PIXEL_OFFSET);
-        Point2D.Float sourcePoint      = distortion.getSourcePoint(currentDestPixel, imageBounds);
-        Color pixelColour              = interpolator.getColourAt(src, sourcePoint);
-        dest.setRGB(i, j, pixelColour.getRGB());
-      }
-    }
+    DistortionProcessor processor = new DistortionProcessor(src, dest);
+    RecursiveAction processAction = new RecursiveImageAction(processor);
+    processAction.invoke();
   }
 
 //------------------------------------------------------------------------------
 
+//******************************************************************************
+
+  private class DistortionProcessor implements ImageProcessor
+  {
+    private BufferedImage src;
+    private BufferedImage dest;
+    private Dimension2D imageBounds;
+
+    public DistortionProcessor(BufferedImage src, BufferedImage dest)
+    {
+      this.src  = src;
+      this.dest = dest;
+
+      imageBounds = new Dimension(dest.getWidth(), dest.getHeight());
+    }
+
+    @Override
+    public int getImageWidth() {return src.getWidth();}
+
+    @Override
+    public int getImageHeight() {return src.getHeight();}
+
+    @Override
+    public void processPixel(int x, int y)
+    {
+      Point2D.Float currentDestPixel = new Point2D.Float(x + PixelizationConstants.PIXEL_OFFSET,
+                                                         y + PixelizationConstants.PIXEL_OFFSET);
+      Point2D.Float sourcePoint      = distortion.getSourcePoint(currentDestPixel, imageBounds);
+      Color pixelColour              = interpolator.getColourAt(src, sourcePoint);
+      dest.setRGB(x, y, pixelColour.getRGB());
+    }
+  }
 }
