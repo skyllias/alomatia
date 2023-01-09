714469d32340d229dbda27f0292a9357bc76e38a
Unknown <31156063+skyllias@users.noreply.github.com>
2023-01-09 09:14:06 +0100

    Refurbish URL download controls
    #139

--- src/main/java/org/skyllias/alomatia/ui/source/UrlDownloadSourceSelectionComposer.java
+++ src/main/java/org/skyllias/alomatia/ui/source/UrlDownloadSourceSelectionComposer.java
@@ -1,36 +1,53 @@
 package org.skyllias.alomatia.ui.source;
 
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
+import org.apache.commons.lang.StringUtils;
 import org.skyllias.alomatia.ImageSource;
+import org.skyllias.alomatia.i18n.LabelLocalizer;
+import org.skyllias.alomatia.preferences.DownloadPreferences;
 import org.skyllias.alomatia.source.AsynchronousUrlSource;
-import org.skyllias.alomatia.ui.UrlDownloadSubcomponentComposer;
-import org.skyllias.alomatia.ui.UrlDownloadSubcomponentComposer.UrlDownloadSubcomponent;
+import org.skyllias.alomatia.source.AsynchronousUrlSource.DownloadListener;
+import org.skyllias.alomatia.ui.component.PathTextField;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-/** Composer for a selector of {@link AsynchronousUrlSource}.
- *  TODO: Refactor when the URL download subcomponent is modified (most
- *  probably, that class will disappear and the controls will come here). */
+/** Composer for a selector of {@link AsynchronousUrlSource}. */
 
 @Component
 @Order(4)
 public class UrlDownloadSourceSelectionComposer implements SourceSelectionComposer
 {
+  protected static final String FIELD_NAME  = "field.url";
+  protected static final String BUTTON_NAME = "button.download";
+
   private static final String SOURCE_KEY = "url";
 
+  private static final String BUTTON_READY_LABEL   = "source.selector.url.button.ready";
+  private static final String BUTTON_CANCEL_LABEL  = "source.selector.url.button.cancel";
+  private static final String ERROR_TOOLTIP_PREFIX = "source.selector.url.field.tooltip.error.";
+
   private final AsynchronousUrlSource urlSource;
-  private final UrlDownloadSubcomponentComposer urlDownloadSubcomponentComposer;
+  private final LabelLocalizer labelLocalizer;
+  private final DownloadPreferences preferences;
 
 //==============================================================================
 
   public UrlDownloadSourceSelectionComposer(AsynchronousUrlSource urlSource,
-                                            UrlDownloadSubcomponentComposer urlDownloadSubcomponentComposer)
+                                            LabelLocalizer labelLocalizer,
+                                            DownloadPreferences preferences)
   {
-    this.urlSource = urlSource;
-    this.urlDownloadSubcomponentComposer = urlDownloadSubcomponentComposer;
+    this.urlSource      = urlSource;
+    this.labelLocalizer = labelLocalizer;
+    this.preferences    = preferences;
   }
 
 //==============================================================================
@@ -38,7 +55,7 @@ public class UrlDownloadSourceSelectionComposer implements SourceSelectionCompos
   @Override
   public SourceSelection buildSelector()
   {
-    return new UrlSourceSelection(urlDownloadSubcomponentComposer.getUrlDownloadSubcomponent(urlSource));
+    return new UrlSourceSelection();
   }
 
 //------------------------------------------------------------------------------
@@ -50,19 +67,32 @@ public class UrlDownloadSourceSelectionComposer implements SourceSelectionCompos
 
 //******************************************************************************
 
-  private class UrlSourceSelection implements SourceSelection
+  private class UrlSourceSelection implements SourceSelection, DownloadListener
   {
-    private final UrlDownloadSubcomponent urlDownloadSubcomponent;
+    private final UrlTextField urlField = new UrlTextField();
+    private final JButton button        = new JButton();
     private final JPanel controlsPanel;
 
-    public UrlSourceSelection(UrlDownloadSubcomponent urlDownloadSubcomponent)
-    {
-      this.urlDownloadSubcomponent = urlDownloadSubcomponent;
+    private final State state = new State();
 
+    public UrlSourceSelection()
+    {
       controlsPanel = new JPanel();
       controlsPanel.setLayout(new BoxLayout(controlsPanel, BoxLayout.X_AXIS));
-      controlsPanel.add(urlDownloadSubcomponent.getTextField());
-      controlsPanel.add(urlDownloadSubcomponent.getButton());
+      controlsPanel.add(urlField);
+      controlsPanel.add(button);
+
+      enableComponents(false);
+      urlField.setName(FIELD_NAME);
+      button.setName(BUTTON_NAME);
+      button.setText(labelLocalizer.getString(BUTTON_READY_LABEL));
+
+      String initialUrl = preferences.getLastUrl();
+      if (initialUrl != null) urlField.setText(initialUrl);
+
+      Action urlAction = new UrlAction(this);
+      button.addActionListener(urlAction);
+      urlField.addActionListener(urlAction);                                    // a JTextField action listener is automatically fired when the enter key is pressed
     }
 
     @Override
@@ -74,13 +104,139 @@ public class UrlDownloadSourceSelectionComposer implements SourceSelectionCompos
         public void setActive(boolean active)
         {
           urlSource.setActive(active);
-          urlDownloadSubcomponent.setEnabled(active);
+          setEnabled(active);
         }
       };
     }
 
     @Override
     public JComponent getControls() {return controlsPanel;}
+
+    public void setEnabled(boolean active)
+    {
+      enableComponents(active);
+
+      if (active) startDownload();
+      else        cancelDownload();
+    }
+
+    /** Changes the button label and the downloading status. */
+
+    @Override
+    public void onSuccess()
+    {
+      state.isDownloading = false;
+      SwingUtilities.invokeLater(new Runnable()
+      {
+        @Override
+        public void run() {button.setText(labelLocalizer.getString(BUTTON_READY_LABEL));}
+      });
+    }
+
+    /** Changes the button label and adds a tooltip to the text field with a
+     *  description of what caused the requested image not to display. */
+
+    @Override
+    public void onError(final ErrorType type)
+    {
+      state.isDownloading = false;
+      SwingUtilities.invokeLater(new Runnable()
+      {
+        @Override
+        public void run()
+        {
+          button.setText(labelLocalizer.getString(BUTTON_READY_LABEL));
+
+          if (type != null)
+          {
+            String errorKey = ERROR_TOOLTIP_PREFIX + type.toString().toLowerCase();
+            urlField.setToolTipText(labelLocalizer.getString(errorKey));
+          }
+        }
+      });
+    }
+
+    /* Changes the enable status of the button and field. */
+
+    private void enableComponents(boolean active)
+    {
+      button.setEnabled(active);
+      urlField.setEnabled(active);
+    }
+
+    /* Clears any tooltip from the text field. */
+
+    private void startDownload()
+    {
+      String url = urlField.getText().trim();
+
+      if (StringUtils.isNotBlank(url))
+      {
+        preferences.setLastUrl(url);
+
+        button.setText(labelLocalizer.getString(BUTTON_CANCEL_LABEL));
+        state.isDownloading = true;
+        urlSource.setUrl(url, this);
+
+        button.requestFocusInWindow();
+        urlField.setToolTipText(null);
+      }
+    }
+
+    private void cancelDownload()
+    {
+      button.setText(labelLocalizer.getString(BUTTON_READY_LABEL));
+      state.isDownloading = false;
+      urlSource.cancel();
+
+      urlField.requestFocusInWindow();                                            // this will not have any effect when this method is called from setEnabled(false)
+    }
+
+  }
+
+//******************************************************************************
+
+  /* Action executed when the button is clicked (either in the ready or the
+   * downloading status) or when the enter is pressed in the urlField. */
+
+  @SuppressWarnings("serial")
+  private class UrlAction extends AbstractAction
+  {
+    private final UrlSourceSelection urlSourceSelection;
+
+
+    public UrlAction(UrlSourceSelection urlSourceSelection)
+    {
+      this.urlSourceSelection = urlSourceSelection;
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+      if (urlSourceSelection.state.isDownloading) urlSourceSelection.cancelDownload();
+      else                                        urlSourceSelection.startDownload();
+    }
+  }
+
+//******************************************************************************
+
+  /* Text field to write URLs in. */
+
+  @SuppressWarnings("serial")
+  private static class UrlTextField extends PathTextField
+  {
+    public UrlTextField()
+    {
+      setEditable(true);
+    }
+  }
+
+//******************************************************************************
+
+  private static class State
+  {
+    boolean isDownloading = false;
   }
 
 //******************************************************************************
---
+++ src/test/java/org/skyllias/alomatia/ui/source/UrlDownloadSourceSelectionComposerTest.java
@@ -0,0 +1,236 @@
+
+package org.skyllias.alomatia.ui.source;
+
+import static org.assertj.swing.fixture.Containers.showInFrame;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.awt.event.KeyEvent;
+import java.util.concurrent.Callable;
+
+import org.assertj.swing.core.KeyPressInfo;
+import org.assertj.swing.edt.FailOnThreadViolationRepaintManager;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.fixture.JButtonFixture;
+import org.assertj.swing.fixture.JTextComponentFixture;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.skyllias.alomatia.i18n.KeyLabelLocalizer;
+import org.skyllias.alomatia.preferences.DownloadPreferences;
+import org.skyllias.alomatia.source.AsynchronousUrlSource;
+import org.skyllias.alomatia.source.AsynchronousUrlSource.DownloadListener;
+import org.skyllias.alomatia.source.AsynchronousUrlSource.DownloadListener.ErrorType;
+
+/* Although there should not be any difference, real URLs are not used because
+ * AssertJ does not support writing strings with chars that are typed with
+ *  modifiers (shift, alt, etc.). */
+
+@RunWith(MockitoJUnitRunner.class)
+public class UrlDownloadSourceSelectionComposerTest
+{
+  @Mock
+  private AsynchronousUrlSource urlSource;
+  @Mock
+  private DownloadPreferences preferences;
+  @Spy
+  private KeyLabelLocalizer labelLocalizer;
+
+  @InjectMocks
+  private UrlDownloadSourceSelectionComposer urlDownloadSourceSelectionComposer;
+
+  @Captor
+  private ArgumentCaptor<DownloadListener> downloadListenerCaptor;
+
+  private FrameFixture frameFixture;
+
+  @BeforeClass
+  public static void setUpOnce()
+  {
+    FailOnThreadViolationRepaintManager.install();
+  }
+
+  private SourceSelection setUpUi()
+  {
+    SourceSelection sourceSelection = GuiActionRunner.execute(new Callable<SourceSelection>()
+    {
+      @Override
+      public SourceSelection call() throws Exception
+      {
+        return urlDownloadSourceSelectionComposer.buildSelector();
+      }
+    });
+    frameFixture = showInFrame(sourceSelection.getControls());
+
+    return sourceSelection;
+  }
+
+  @After
+  public void tearDown()
+  {
+    frameFixture.cleanUp();
+  }
+
+//------------------------------------------------------------------------------
+
+  @Test
+  public void shouldStartDisabled()
+  {
+    setUpUi();
+
+    getField().requireText("");
+    getField().requireDisabled();
+    getButton().requireDisabled();
+  }
+
+  @Test
+  public void shouldInitializeFieldIfUrlInPreferences()
+  {
+    when(preferences.getLastUrl()).thenReturn("url from preferences");
+
+    setUpUi();
+
+    getField().requireText("url from preferences");
+    getField().requireDisabled();
+    getButton().requireDisabled();
+  }
+
+  @Test
+  public void shouldEnableControlsWhenSourceActivated()
+  {
+    SourceSelection sourceSelection = setUpUi();
+    GuiActionRunner.execute(() -> sourceSelection.getSource().setActive(true));
+
+    getField().requireEnabled();
+    getButton().requireEnabled();
+    verify(urlSource).setActive(true);
+  }
+
+  @Test
+  public void shouldDisableControlsWhenSourceDeactivated()
+  {
+    SourceSelection sourceSelection = setUpUi();
+    GuiActionRunner.execute(() -> sourceSelection.getSource().setActive(true));
+    GuiActionRunner.execute(() -> sourceSelection.getSource().setActive(false));
+
+    getField().requireDisabled();
+    getButton().requireDisabled();
+    verify(urlSource).setActive(false);
+  }
+
+  @Test
+  public void shouldNotStartDownloadWhenFieldEmpty()
+  {
+    SourceSelection sourceSelection = setUpUi();
+    GuiActionRunner.execute(() -> sourceSelection.getSource().setActive(true));
+
+    getButton().click();
+
+    verify(urlSource, never()).setUrl(any(), any());
+    getButton().requireText("source.selector.url.button.ready");
+  }
+
+  @Test
+  public void shouldStartDownloadWhenUrlAvailable()
+  {
+    SourceSelection sourceSelection = setUpUi();
+    GuiActionRunner.execute(() -> sourceSelection.getSource().setActive(true));
+
+    getField().enterText("url to download");
+    getButton().click();
+
+    verify(urlSource).setUrl(eq("url to download"), any());
+    verify(urlSource, never()).cancel();
+    verify(preferences).setLastUrl("url to download");
+    getButton().requireText("source.selector.url.button.cancel");
+    getField().requireToolTip((String) null);
+  }
+
+  @Test
+  public void shouldStartDownloadOnEnterPressToo()
+  {
+    SourceSelection sourceSelection = setUpUi();
+    GuiActionRunner.execute(() -> sourceSelection.getSource().setActive(true));
+
+    getField().enterText("url to download");
+    getField().pressAndReleaseKey(KeyPressInfo.keyCode(KeyEvent.VK_ENTER));
+
+    verify(urlSource).setUrl(eq("url to download"), any());
+    verify(urlSource, never()).cancel();
+    verify(preferences).setLastUrl("url to download");
+    getButton().requireText("source.selector.url.button.cancel");
+    getField().requireToolTip((String) null);
+  }
+
+  @Test
+  public void shouldCancelDownloadWhenButtonClickedWhileDownloading()
+  {
+    SourceSelection sourceSelection = setUpUi();
+    GuiActionRunner.execute(() -> sourceSelection.getSource().setActive(true));
+
+    getField().enterText("url to download");
+    getButton().click();
+    getButton().click();
+
+    verify(urlSource).setUrl(eq("url to download"), any());
+    verify(urlSource).cancel();
+    getButton().requireText("source.selector.url.button.ready");
+    getField().requireToolTip((String) null);
+    getField().requireFocused();
+  }
+
+  @Test
+  public void shouldBeReadyAfterSuccess()
+  {
+    SourceSelection sourceSelection = setUpUi();
+    GuiActionRunner.execute(() -> sourceSelection.getSource().setActive(true));
+
+    getField().enterText("url to download");
+    getButton().click();
+
+    verify(urlSource).setUrl(eq("url to download"), downloadListenerCaptor.capture());
+    downloadListenerCaptor.getValue().onSuccess();
+
+    getButton().requireText("source.selector.url.button.ready");
+    getField().requireToolTip((String) null);
+  }
+
+  @Test
+  public void shouldShowErrorTooltipAfterFailure()
+  {
+    SourceSelection sourceSelection = setUpUi();
+    GuiActionRunner.execute(() -> sourceSelection.getSource().setActive(true));
+
+    getField().enterText("url to download");
+    getButton().click();
+
+    verify(urlSource).setUrl(eq("url to download"), downloadListenerCaptor.capture());
+    downloadListenerCaptor.getValue().onError(ErrorType.IMAGE);
+
+    getButton().requireText("source.selector.url.button.ready");
+    getField().requireToolTip("source.selector.url.field.tooltip.error.image");
+  }
+
+
+  private JTextComponentFixture getField()
+  {
+    return frameFixture.textBox(UrlDownloadSourceSelectionComposer.FIELD_NAME);
+  }
+
+  private JButtonFixture getButton()
+  {
+    return frameFixture.button(UrlDownloadSourceSelectionComposer.BUTTON_NAME);
+  }
+
+}
