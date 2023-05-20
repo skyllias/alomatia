91999e97c49fba3875cf7274dfabbb8744c15404
skyllias <31156063+skyllias@users.noreply.github.com>
2023-05-20 11:04:44 +0200

    Replace theme

--- src/main/java/org/skyllias/alomatia/app/SwingAlomatia.java
+++ src/main/java/org/skyllias/alomatia/app/SwingAlomatia.java
@@ -1,25 +1,23 @@
 
 package org.skyllias.alomatia.app;
 
-import java.util.Properties;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 import org.skyllias.alomatia.dependency.BeanFactoryLoader;
 import org.skyllias.alomatia.ui.controls.ControlsFrameManager;
 
-import com.jtattoo.plaf.aero.AeroLookAndFeel;
+import com.formdev.flatlaf.FlatIntelliJLaf;
 
 /** Launcher of a visual application. */
 
 public class SwingAlomatia
 {
-  private static final String LNF_LOGO_STRING_PROPERTY = "logoString";
-  private static final String APP_NAME                 = "Alomatia";
-
 //==============================================================================
 
   public static void main(String[] args)
@@ -37,10 +35,9 @@ public class SwingAlomatia
       {
         try
         {
-          Properties lnfProperties = new Properties();
-          lnfProperties.put(LNF_LOGO_STRING_PROPERTY, APP_NAME);
-          AeroLookAndFeel.setCurrentTheme(lnfProperties);
-          UIManager.setLookAndFeel(new AeroLookAndFeel());                      // TODO make it optional
+          JFrame.setDefaultLookAndFeelDecorated(true);
+          JDialog.setDefaultLookAndFeelDecorated(true);
+          UIManager.setLookAndFeel(new FlatIntelliJLaf());
 
           controlFrameController.get().createControlFrame();
         }
