fae2db88f1c54c5134c7a8c3abf92a7076f47339
skyllias <31156063+skyllias@users.noreply.github.com>
2023-09-29 18:27:39 +0200

    Smooth B&W filters with Gaussian blur  #141

--- src/main/java/org/skyllias/alomatia/filter/buffered/surround/SurroundingFilterFactory.java
+++ src/main/java/org/skyllias/alomatia/filter/buffered/surround/SurroundingFilterFactory.java
@@ -7,6 +7,8 @@ import java.util.Random;
 
 import org.skyllias.alomatia.filter.buffered.HintlessBufferedImageOp;
 import org.skyllias.alomatia.filter.buffered.SingleFrameBufferedImageFilter;
+import org.skyllias.alomatia.filter.compose.ComposedFilter;
+import org.skyllias.alomatia.filter.convolve.BlurFilterFactory;
 
 /** Instantiator of filters that work with the surroundings of each pixel. */
 
@@ -14,30 +16,51 @@ public class SurroundingFilterFactory
 {
 //==============================================================================
 
+  public static ImageFilter forStrictBlackAndWhite(Color blackColor, Color whiteColor)
+  {
+    return forStrictBlackAndWhite(0, 0.5f, blackColor, whiteColor);
+  }
+
+//------------------------------------------------------------------------------
+
+  public static ImageFilter forStrictBlackAndWhite(int blurSize, Color blackColor, Color whiteColor)
+  {
+    return forStrictBlackAndWhite(blurSize, 0.5f, blackColor, whiteColor);
+  }
+
+//------------------------------------------------------------------------------
+
+  public static ImageFilter forStrictBlackAndWhite(int blurSize, float threshold,
+                                                   Color blackColor, Color whiteColor)
+  {
+    return forPreBlurredBlackAndWhite(blurSize, new StrictBlackOrWhiteSelector(threshold, blackColor, whiteColor));
+  }
+
+//------------------------------------------------------------------------------
+
+  public static ImageFilter forProbabilisticBlackOrWhite(int blurSize)
+  {
+    return forPreBlurredBlackAndWhite(blurSize, new ProbabilisticBlackOrWhiteSelector(new Random()));
+  }
+
+//------------------------------------------------------------------------------
+
+  private static ImageFilter forPreBlurredBlackAndWhite(int blurSize, BlackOrWhiteSelector blackOrWhiteSelector)
+  {
+    LightCalculator lightCalculator = new LightCalculator(blackOrWhiteSelector);
+    HintlessBufferedImageOp imageOp = new HintlessBufferedImageOp(new SurroundingColoursOperation(0, lightCalculator));
+
+    SingleFrameBufferedImageFilter blackOrWhiteFilter = new SingleFrameBufferedImageFilter(imageOp);
+    if (blurSize == 0) return blackOrWhiteFilter;
+    else               return new ComposedFilter(BlurFilterFactory.forGaussian(blurSize),
+                                                 blackOrWhiteFilter);
+  }
+
+//------------------------------------------------------------------------------
+
   public static ImageFilter forMedian(int boxSize)
   {
     return forCalculator(boxSize, new MedianChannelCalculator());
-  }
-
-//------------------------------------------------------------------------------
-
-  public static ImageFilter forStrictBlackOrWhite(int boxSize, Color blackColor, Color whiteColor)
-  {
-    return forStrictBlackOrWhite(boxSize,  0.5f, blackColor, whiteColor);
-  }
-
-//------------------------------------------------------------------------------
-
-  public static ImageFilter forStrictBlackOrWhite(int boxSize, float threshold, Color blackColor, Color whiteColor)
-  {
-    return forCalculator(boxSize, new LightCalculator(new StrictBlackOrWhiteSelector(threshold, blackColor, whiteColor)));
-  }
-
-//------------------------------------------------------------------------------
-
-  public static ImageFilter forProbabilisticBlackOrWhite(int boxSize)
-  {
-    return forCalculator(boxSize, new LightCalculator(new ProbabilisticBlackOrWhiteSelector(new Random())));
   }
 
 //------------------------------------------------------------------------------
