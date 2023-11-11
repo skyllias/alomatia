ec5eaf0473b13e61ffc888537e13eb7ccd1aad5f
skyllias <31156063+skyllias@users.noreply.github.com>
2023-11-11 17:14:09 +0100

    Store screen capture bounds in preferences

--- src/main/java/org/skyllias/alomatia/preferences/SourcePreferences.java
+++ src/main/java/org/skyllias/alomatia/preferences/SourcePreferences.java
@@ -1,6 +1,7 @@
 
 package org.skyllias.alomatia.preferences;
 
+import java.awt.Rectangle;
 import java.util.prefs.Preferences;
 
 import org.springframework.stereotype.Component;
@@ -15,6 +16,10 @@ public class SourcePreferences
   private static final String PREFKEY_CLIPBOARDAUTO = "sourceClipboardAuto";
   private static final String PREFKEY_DEFAULTDIR    = "defaultSourceDir";
   private static final String PREFKEY_DEFAULTFILE   = "defaultSourceFile";
+  private static final String PREFKEY_SCREEN_RECT_X = "screenRectanlgeX";
+  private static final String PREFKEY_SCREEN_RECT_Y = "screenRectanlgeY";
+  private static final String PREFKEY_SCREEN_RECT_W = "screenRectanlgeWidth";
+  private static final String PREFKEY_SCREEN_RECT_H = "screenRectanlgeHeight";
 
   private final Preferences preferences = Preferences.userNodeForPackage(getClass());
 
@@ -72,6 +77,26 @@ public class SourcePreferences
   public void setDefaultDirPath(String dirPath)
   {
     preferences.put(PREFKEY_DEFAULTDIR, dirPath);
+  }
+
+//------------------------------------------------------------------------------
+
+  public Rectangle getScreenRectangle()
+  {
+    return new Rectangle(preferences.getInt(PREFKEY_SCREEN_RECT_X, 0),
+                         preferences.getInt(PREFKEY_SCREEN_RECT_Y, 0),
+                         preferences.getInt(PREFKEY_SCREEN_RECT_W, 600),
+                         preferences.getInt(PREFKEY_SCREEN_RECT_H, 450));
+  }
+
+//------------------------------------------------------------------------------
+
+  public void setScreenRectangle(Rectangle screenRectangle)
+  {
+    preferences.putInt(PREFKEY_SCREEN_RECT_X, screenRectangle.x);
+    preferences.putInt(PREFKEY_SCREEN_RECT_Y, screenRectangle.y);
+    preferences.putInt(PREFKEY_SCREEN_RECT_W, screenRectangle.width);
+    preferences.putInt(PREFKEY_SCREEN_RECT_H, screenRectangle.height);
   }
 
 //------------------------------------------------------------------------------
